6571304515fae2a349f40d749f0ffbfb95603a85
Brian Beltran <brian-adrian22@hotmail.com>
2025-01-31 22:54:05 -0700

    Agregando Boostrap, img y css

--- carritoAutomovil/src/App.jsx
+++ carritoAutomovil/src/App.jsx
@@ -1,33 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <header className="py-3 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <img className="img-fluid" src="./public/img/Mustang-LOGO.svg" alt="imagen logo" />
+            </a>
+          </div>
+          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito-container">
+              <div className="carrito">
+                <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                <div id="carrito-content" className="carrito-content"><p className="text-center">El carrito está vacío</p>                  
+                  <table className="w-100 table">
+                    <thead>
+                      <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                      </tr>
+                    </thead>
+                    <tbody>                      
+                      <tr>
+                        <td className="align-middle">
+                          <img className="img-fluid" src="./public/img/aspark owl.jpg" alt="imagen automóvil" />
+                        </td>
+                        <td className="align-middle">Nombre</td>
+                        <td className="fw-bold align-middle">$1.000.000</td>
+                        <td className="align-middle">
+                          <div className="d-flex align-items-center gap-4">
+                            <button 
+                              type="button" 
+                              className="btn btn-dark"
+                              >
+                                -
+                              </button>
+                            1
+                            <button 
+                              type="button" 
+                              className="btn btn-dark"
+                              >
+                                +
+                              </button>
+                          </div>
+                        </td>
+                        <td className="align-middle border-top-0">
+                          <button 
+                            type="button" 
+                            className="btn-close" 
+                            aria-label="Close"
+                            ></button>
+                        </td>
+                      </tr>
+                    </tbody>
+
+                  </table>
+                  <p className="text-end">Total a pagar: <span className="fw-bold">$ 1.000.000</span></p>
+                  <button 
+                    className="btn btn-dark w-100 mt-3 p-2"
+                    >Vaciar Carrito</button>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </header>
+
+    <main className="container-xl mt-5">
+      <h2 className="text-center">Automóviles disponibles</h2>
+      <div className="row mt-5"></div>
+      
+      <div className="col-md-8 col-lg-4 my-4 row align-items-center mx-auto">        
+        <div className="col-10 row align-items-center mx-auto">
+          <img className="img-fluid" src="./public/img/aspark owl.jpg" alt="imagen automóvil" />  
+          <h3 className="text-black fs-4 fw-bold text-uppercase">Bugatti Chiron</h3>
+          <p>Bugatti</p>
+          <p>420 km/h</p>
+          <p>2016</p>
+          <p className="fw-black text-primary fs-3">U$S - 3.300.000</p> 
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+            >Agregar al Carrito</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
+
+    <footer className="mt-5 py-5">
+      <div className="container-xl">
+        <p className="text-center fs-6 mt-4 m-md-0 text-muted">© 2024 Super Carros, Inc</p>
+      </div>
+    </footer>
     </>
   )
 }
